bc875e49a043134f67a288da2ff4fcd28c260648
battlesocce <70088282+battlesocce@users.noreply.github.com>
2020-09-01 01:37:16 +0530

    Update settings.py

--- battlesoccer/battlesoccer/settings.py
+++ battlesoccer/battlesoccer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!k4cbyp4ui(ls&mck*kosuql9(mpy)snmakwmcfrta=!5-33(k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
